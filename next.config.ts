6bfa3c28ec775633f2427a844ad69a716862d6b1
M.A.S. Ashikur Rahman Hridoy <125389825+b1ankZer0@users.noreply.github.com>
2026-01-24 13:44:37 +0600

    Update server initialization in next.config.ts
    Conditionally call the server function based on environment variables.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import { server } from "@/backend/server";
 import type { NextConfig } from "next";
 
-server();
+// server();
+if (process.env.NODE_ENV !== 'production' || process.env.NEXT_PHASE !== 'phase-production-build') {
+    server()
+}
 
 const nextConfig: NextConfig = {
   // Tell Next.js to ignore TS errors during production builds
